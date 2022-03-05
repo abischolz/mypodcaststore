62766a038286d1a39310853f97f25abd4fba3456
abischolz <Abi.scholz@gmail.com>
2022-03-05 17:35:49 -0500

    Initial commit

---
+++ src/main/java/com/example/myPodcastStore/MyPodcastStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.myPodcastStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPodcastStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPodcastStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myPodcastStore/MyPodcastStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myPodcastStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPodcastStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
