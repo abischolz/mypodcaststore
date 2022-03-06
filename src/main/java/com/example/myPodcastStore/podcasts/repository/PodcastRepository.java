19050224f20be3dc241c600fd8107e62db8328cc
abischolz <Abi.scholz@gmail.com>
2022-03-06 12:36:40 -0500

    Initialized CRUD repo

--- src/main/java/com/example/myPodcastStore/podcasts/repository/PodcastRepository.java
+++ src/main/java/com/example/myPodcastStore/podcasts/repository/PodcastRepository.java
@@ -1,6 +1,7 @@
 package com.example.myPodcastStore.podcasts.repository;
 
+import com.example.myPodcastStore.podcasts.model.Podcast;
 import org.springframework.data.repository.CrudRepository;
-// need to add type so crudrepository knows what it is managing
-//public interface PodcastRepository extends CrudRepository {
-//}
+
+public interface PodcastRepository extends CrudRepository <Podcast, Integer> {
+}
