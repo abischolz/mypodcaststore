c2a182eacf7a5b8fb63f1ad05f7fbde4947ca882
abischolz <Abi.scholz@gmail.com>
2022-03-06 15:04:32 -0500

    Dependency injection - user controller

--- src/main/java/com/example/myPodcastStore/podcasts/repository/UserRepository.java
+++ src/main/java/com/example/myPodcastStore/podcasts/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.example.myPodcastStore.podcasts.repository;
 
-public interface UserRepository {
+import com.example.myPodcastStore.podcasts.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository <User, Integer> {
 }
